086dd223269a93a426aa10a74979b53bb52c94d3
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-12-03 10:29:17 +0800

    add

--- src/main/java/collections/ArrayListDemo.java
+++ src/main/java/collections/ArrayListDemo.java
@@ -2,6 +2,7 @@ package collections;
 
 public class ArrayListDemo {
     private static final Object[] EMPTY_ELEMENT_DATA = {};
+    private static final int DEFAULT_CAPACITY = 10;
     private Object[] elementData;
     private int size;
     private int capacity;
@@ -11,47 +12,80 @@ public class ArrayListDemo {
     }
 
     public ArrayListDemo(int capacity) {
-        if(capacity > 0) {
+        if (capacity > 0) {
             this.capacity = capacity;
             this.elementData = new Object[capacity];
         }
-        if(capacity == 0) {
+        if (capacity == 0) {
             this.elementData = EMPTY_ELEMENT_DATA;
         }
-        if(capacity < 0) {
+        if (capacity < 0) {
             throw new IllegalArgumentException("Illegal capacity: " + capacity);
         }
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public boolean add(Object obj){
-        return false;
+    public boolean add(Object obj) {
+        if (elementData == EMPTY_ELEMENT_DATA) {
+            elementData = new Object[DEFAULT_CAPACITY];
+            this.capacity = DEFAULT_CAPACITY;
+        }
+        if (size == capacity) {
+            resize();
+        }
+
+        elementData[size++] = obj;
+        return true;
     }
 
-    public void add(int index, Object obj){
+    public boolean add(int index, Object obj) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index is out of bounds, index: " + index + "size: " + size);
+        }
+        if (size == capacity) {
+            resize();
+        }
 
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = obj;
+        size++;
+        return true;
     }
 
-    public Object remove(int index){
+    private void resize() {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity * 2;
+        Object[] newElementData = new Object[newCapacity];
+
+        System.arraycopy(elementData, 0, newElementData, 0, oldCapacity);
+        elementData = newElementData;
+        capacity = newCapacity;
+    }
+
+    public Object remove(int index) {
         return null;
     }
 
-    public boolean remove(Object value){
+    public boolean remove(Object value) {
         return false;
     }
 
-    public Object get(int index){
+    public Object get(int index) {
         return null;
     }
 
-    public void set(int index, Object obj){
+    public int indexOf(Object obj) {
+        return -1;
+    }
+
+    public void set(int index, Object obj) {
 
     }
 }
--- src/test/java/collections/ArrayListDemoTest.java
+++ src/test/java/collections/ArrayListDemoTest.java
@@ -1,7 +1,57 @@
 package collections;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class ArrayListDemoTest {
+    private ArrayListDemo list;
 
+    @Before
+    public void initialize() {
+        list = new ArrayListDemo();
+    }
+
+    @Test
+    public void isEmpty() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void add() {
+        list.add(1);
+        list.add("abs");
+        list.add(new Date());
+    }
+
+    @Test
+    public void add1() {
+    }
+
+    @Test
+    public void remove() {
+    }
+
+    @Test
+    public void remove1() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void indexOf() {
+    }
+
+    @Test
+    public void set() {
+    }
 }
