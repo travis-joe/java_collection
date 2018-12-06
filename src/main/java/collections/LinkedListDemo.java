0cfd51c8e4653e5e8ad18d366a8d16b7866c92ff
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-12-06 17:27:51 +0800

    linkedList add

--- src/main/java/collections/LinkedListDemo.java
+++ src/main/java/collections/LinkedListDemo.java
@@ -30,6 +30,95 @@ public class LinkedListDemo {
         return true;
     }
 
+    public void add(int index, Object obj) {
+        if (!checkPositionIndex(index)) {
+            throw new IndexOutOfBoundsException("Index is invalid, index:" + index);
+        }
+        if (index == size) {
+            linkLast(obj);
+        } else {
+            linkBefore(obj, traverse(index));
+        }
+    }
 
+    private void linkFirst(Object obj) {
+        if (first != null) {
+            Node oldFirst = first;
+            Node node = new Node(null, obj, oldFirst);
+            oldFirst.prev = node;
+            first = node;
+        } else {
+            Node node = new Node(null, obj, null);
+            first = node;
+            last = node;
+        }
+
+        size++;
+    }
+
+    private void linkLast(Object obj) {
+        if (last != null) {
+            Node oldLast = last;
+            Node node = new Node(oldLast, obj, null);
+            oldLast.next = node;
+            last = node;
+        } else {
+            Node node = new Node(null, obj, null);
+            first = node;
+            last = node;
+        }
+
+        size++;
+
+    }
+
+    private void linkBefore(Object obj, Node node) {
+        Node preNode = node.prev;
+        Node newNode = new Node(preNode, obj, node);
+        node.prev = newNode;
+        if (preNode == null) {
+            first = newNode;
+        } else {
+            preNode.next = newNode;
+        }
+        size++;
+    }
+
+    private boolean checkPositionIndex(int index) {
+        return index >= 0 && index <= size;
+    }
+
+    Node traverse(int index) {
+        if (index < size / 2) {
+            return traverseAhead(index);
+        } else {
+            return traverseReverse(index);
+        }
+    }
+
+    private Node traverseAhead(int index) {
+        Node x = first;
+        for (int i = 0; i < index; i++) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    private Node traverseReverse(int index) {
+        Node x = last;
+        for (int i = size - 1; i > index; i--) {
+            x = x.prev;
+        }
+        return x;
+    }
+
+
+    public Object remove(int index){
+
+    }
+
+    public boolean remore(Object obj) {
+
+    }
 }
 
